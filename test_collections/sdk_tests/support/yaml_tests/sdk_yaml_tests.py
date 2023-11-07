c8ead7097efcc8c42d386687c011868afbbd5ceb
hiltonlima <116589806+hiltonlima@users.noreply.github.com>
2023-11-07 11:35:04 -0300

    [Feature] Added Custom YAMLs (#14)
    * Added custom yamls tests
    
    * add custom folder
    
    * Add blank line
    
    * review changes

--- test_collections/sdk_tests/support/yaml_tests/sdk_yaml_tests.py
+++ test_collections/sdk_tests/support/yaml_tests/sdk_yaml_tests.py
@@ -41,6 +41,10 @@ from .models.yaml_test_parser import YamlParserException, parse_yaml_test
 SDK_YAML_PATH = Path("/app/backend/test_collections/sdk_tests/sdk_checkout/yaml_tests/yaml/sdk")
 SDK_YAML_TEST_FOLDER = YamlTestFolder(path=SDK_YAML_PATH, filename_pattern="Test_TC*")
 
+CUSTOM_YAML_PATH = Path("/app/backend/test_collections/sdk_tests/sdk_checkout/yaml_tests/yaml/custom")
+CUSTOM_YAML_TEST_FOLDER = YamlTestFolder(path=CUSTOM_YAML_PATH, filename_pattern="Test_TC*")
+
+
 def _init_test_suites(yaml_version: str) -> dict[SuiteType, YamlSuiteDeclaration]:
     return {
         SuiteType.MANUAL: YamlSuiteDeclaration(
@@ -68,7 +72,7 @@ def _parse_yaml_to_test_case_declaration(
     return YamlCaseDeclaration(test=yaml_test, yaml_version=yaml_version)
 
 
-def _parse_all_sdk_yaml(
+def _parse_all_yaml(
     yaml_files: list[Path], yaml_version: str
 ) -> list[YamlSuiteDeclaration]:
     """Parse all yaml files and organize them in the 3 test suites:
@@ -108,10 +112,33 @@ def sdk_yaml_test_collection(
 
     files = yaml_test_folder.yaml_file_paths()
     version = yaml_test_folder.version
-    suites = _parse_all_sdk_yaml(yaml_files=files, yaml_version=version)
+    suites = _parse_all_yaml(yaml_files=files, yaml_version=version)
 
     for suite in suites:
         suite.sort_test_cases()
         collection.add_test_suite(suite)
+
+    return collection
+
+
+def custom_yaml_test_collection(
+    yaml_test_folder: YamlTestFolder = CUSTOM_YAML_TEST_FOLDER,
+) -> YamlCollectionDeclaration:
+    """Declare a new collection of test suites."""
+    collection = YamlCollectionDeclaration(
+        name="Custom YAML Tests", folder=yaml_test_folder
+    )
+
+    files = yaml_test_folder.yaml_file_paths()
+    suites = _parse_all_yaml(yaml_files=files, yaml_version=None)
+
+    for suite in suites:
+        if not suite.test_cases:
+            continue
+        suite.sort_test_cases()
+        collection.add_test_suite(suite)
+
+    if not collection.test_suites:
+        return None
 
     return collection
