223d7ab699611d485dae8f8ef1b53c89ff09b326
Carolina Lopes <116589288+ccruzagralopes@users.noreply.github.com>
2023-11-13 10:39:23 -0300

    Update SDK SHA (#25)

--- app/core/config.py
+++ app/core/config.py
@@ -113,9 +113,9 @@ class Settings(BaseSettings):
 
     # SDK Docker Image
     SDK_DOCKER_IMAGE: str = "connectedhomeip/chip-cert-bins"
-    SDK_DOCKER_TAG: str = "19771ed7101321d68b87d05201d42d00adf5368f"
+    SDK_DOCKER_TAG: str = "f06d9520d02d68076c5accbf839f168cda89c47c"
     # SDK SHA: used to fetch test YAML from SDK.
-    SDK_SHA: str = "19771ed7101321d68b87d05201d42d00adf5368f"
+    SDK_SHA: str = "f06d9520d02d68076c5accbf839f168cda89c47c"
 
     class Config:
         case_sensitive = True
