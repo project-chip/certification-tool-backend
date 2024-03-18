d2315b934b5740fe479e1a398f38459976519cfa
Romulo Quidute Filho <rquidute@apple.com>
2024-03-18 21:28:46 +0000

    Implement The default program config for Program-specific

--- app/default_environment_config.py
+++ app/default_environment_config.py
@@ -18,14 +18,24 @@ from pathlib import Path
 from app.schemas.test_environment_config import TestEnvironmentConfig
 
 PROJECT_ROOT = Path(__file__).parent.parent
+TEST_COLLECTIONS = "test_collections"
 TEST_ENVIRONMENT_CONFIG_NAME = "default_test_environment.config"
-TEST_ENVIRONMENT_CONFIG_PATH = PROJECT_ROOT / TEST_ENVIRONMENT_CONFIG_NAME
+TEST_ENVIRONMENT_CONFIG_PATH = (
+    PROJECT_ROOT / TEST_COLLECTIONS / TEST_ENVIRONMENT_CONFIG_NAME
+)
+
+# TH  default project config file
+# This will be used only if the program does not provide a default project config file
+TEST_ENVIRONMENT_CONFIG_PATH_TH_DEFAULT = PROJECT_ROOT / TEST_ENVIRONMENT_CONFIG_NAME
+
+config_path = TEST_ENVIRONMENT_CONFIG_PATH
 
 if not TEST_ENVIRONMENT_CONFIG_PATH.is_file():
-    raise RuntimeError("No test environment config found. Recreating from example.")
+    # If the defult project config file is not find, use the TH default
+    config_path = TEST_ENVIRONMENT_CONFIG_PATH_TH_DEFAULT
+    if not TEST_ENVIRONMENT_CONFIG_PATH_TH_DEFAULT.is_file():
+        raise RuntimeError("No test environment config found. Recreating from example.")
 
-default_environment_config = TestEnvironmentConfig.parse_file(
-    TEST_ENVIRONMENT_CONFIG_PATH
-)
+default_environment_config = TestEnvironmentConfig.parse_file(config_path)
 
 default_environment_config.__dict__
