ef09e0b85087d0a934367632bb0e601506499cdb
hiltonlima <116589806+hiltonlima@users.noreply.github.com>
2024-06-20 09:55:01 -0300

    Do not pull SDK image if it exists a local image (#100)
    * do not pull if image exists
    
    * do not redirect error output

--- test_collections/matter/scripts/update-sample-apps.sh
+++ test_collections/matter/scripts/update-sample-apps.sh
@@ -14,6 +14,7 @@
  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  # See the License for the specific language governing permissions and
  # limitations under the License.
+set -e
 MATTER_PROGRAM_DIR=$(realpath $(dirname "$0")/..)
 TH_SCRIPTS_DIR="$MATTER_PROGRAM_DIR/../../../scripts"
 
@@ -24,12 +25,17 @@ print_start_of_script
 print_script_step "Pulling the SDK Docker image"
 # We are fetching SDK docker image and tag name from backend
 # This is done to minimize the places the SDK version is tracked.
-SDK_DOCKER_IMAGE=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_IMAGE | cut -d'"' -f 2 | cut -d"'" -f 2)
+SDK_DOCKER_PACKAGE=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_IMAGE | cut -d'"' -f 2 | cut -d"'" -f 2)
 SDK_DOCKER_TAG=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_TAG | cut -d'"' -f 2 | cut -d"'" -f 2)
-sudo docker pull $SDK_DOCKER_IMAGE:$SDK_DOCKER_TAG
+SDK_DOCKER_IMAGE=$SDK_DOCKER_PACKAGE:$SDK_DOCKER_TAG
+
+if [[ -z "$(docker images -q $SDK_DOCKER_IMAGE)" ]]; then
+    print_script_step "Downloading '$SDK_DOCKER_IMAGE' image"
+    sudo docker pull $SDK_DOCKER_IMAGE
+fi
 
 print_script_step "Updating Sample APPs"
-sudo docker run -t -v ~/apps:/apps $SDK_DOCKER_IMAGE:$SDK_DOCKER_TAG bash -c "rm -v /apps/*; cp -v * /apps/;"
+sudo docker run -t -v ~/apps:/apps $SDK_DOCKER_IMAGE bash -c "rm -v /apps/*; cp -v * /apps/;"
 sudo chown -R `whoami` ~/apps
 
 print_end_of_script
