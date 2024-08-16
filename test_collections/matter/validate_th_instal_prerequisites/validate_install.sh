5a9b54ae07776ba1acc53f4e0ac42dd0c10e2730
Hilton Lima <hilton_lima@apple.com>
2024-08-16 19:20:40 +0000

    Fixing logic

--- test_collections/matter/validate_th_instal_prerequisites/validate_install.sh
+++ test_collections/matter/validate_th_instal_prerequisites/validate_install.sh
@@ -1,3 +1,4 @@
+#! /usr/bin/env bash
 
  #
  # Copyright (c) 2024 Project CHIP Authors
@@ -13,17 +14,25 @@
  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  # See the License for the specific language governing permissions and
  # limitations under the License.
- FROM ubuntu:24.04
- 
- WORKDIR /app
+set -e
 
- COPY . /app/
 
- ADD  https://github.com/project-chip/certification-tool.git#isolate_dependency_package_list /app/certification-tool
+# Usage: ./test_collections/matter/scripts/update-paa-certs.sh
 
- CMD ["./validate_install.sh", "1>", "/dev/null"]
- 
- 
+apt-get update -y
 
- 
- 
+# Paths
+MATTER_PACKAGE_LIST_FILE="/app/certification-tool/backend/test_collections/matter/scripts/package-dependency-list.txt"
+TH_PACKAGE_LIST_FILE="/app/certification-tool/scripts/ubuntu/package-dependency-list.txt"
+
+readarray packagelist < "$MATTER_PACKAGE_LIST_FILE"
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\r")
+for package in ${packagelist[@]}; do
+  echo "Instaling package: ${package[@]}"
+  DEBIAN_FRONTEND=noninteractive apt-get satisfy ${package[@]} -y --allow-downgrades
+done
+IFS=$SAVEIFS 
+
+
