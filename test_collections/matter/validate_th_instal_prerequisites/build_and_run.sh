ce7dead3ebe39780d9d5245f7803e32d423151bf
Hilton Lima <hilton_lima@apple.com>
2024-08-19 17:30:31 +0000

    remove mount parameter

--- test_collections/matter/validate_th_instal_prerequisites/build_and_run.sh
+++ test_collections/matter/validate_th_instal_prerequisites/build_and_run.sh
@@ -15,12 +15,13 @@
  # See the License for the specific language governing permissions and
  # limitations under the License.
 set -e
+set -x
 
 TH_DIR=$(realpath $(dirname "$0")/../../../..)
 CONTAINER_TH_DIR="/app/certification-tool"
 CONTAINER_VALIDATE_SCRIPT="$CONTAINER_TH_DIR/backend/test_collections/matter/validate_th_instal_prerequisites/validate_install.sh"
-DOCKER_IMAGE_TAG="validade_th_installation:v1"
+CONTAINER_NAME="validade_th_installation"
+DOCKER_IMAGE_TAG="$CONTAINER_NAME:v1"
 
 docker build -t $DOCKER_IMAGE_TAG .
 docker run -v $TH_DIR:/app/certification-tool:ro -it $DOCKER_IMAGE_TAG $CONTAINER_VALIDATE_SCRIPT
---mount source=$TH_DIR,destination=$CONTAINER_TH_DIR,readonly
