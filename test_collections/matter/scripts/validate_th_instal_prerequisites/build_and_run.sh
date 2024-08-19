e3cd2dcbb489bc8f16a47630bb8eeb02b62c2a5a
Hilton Lima <hilton_lima@apple.com>
2024-08-19 18:56:58 +0000

    Adding comments

--- test_collections/matter/scripts/validate_th_instal_prerequisites/build_and_run.sh
+++ test_collections/matter/scripts/validate_th_instal_prerequisites/build_and_run.sh
@@ -14,12 +14,22 @@
  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  # See the License for the specific language governing permissions and
  # limitations under the License.
+
+# A way to validate the installation of packages used by the Matter Test
+# harness has been created.
+# This is a common/recurring problem because some packages no longer exist
+# or the installation method changes (Docker, for example).
+# Therefore, we can use this script to periodically validate whether the 
+# packages are available for installation without having to perform a
+# complete system installation.
+#
+# Usage: ./test_collections/matter/scripts/validate_th_instal_prerequisites/build_and_run.sh
+
 set -e
-set -x
 
 TH_DIR=$(realpath $(dirname "$0")/../../../../..)
 CONTAINER_TH_DIR="/app/certification-tool"
-VALIDATE_SCRIPT_DIR="/backend/test_collections/matter/scripts/validate_th_instal_prerequisites"
+VALIDATE_SCRIPT_DIR="backend/test_collections/matter/scripts/validate_th_instal_prerequisites"
 CONTAINER_VALIDATE_SCRIPT="$CONTAINER_TH_DIR/$VALIDATE_SCRIPT_DIR/validate_install.sh"
 DOCKER_IMAGE_TAG="validade_th_installation:v1"
 
--- test_collections/matter/scripts/validate_th_instal_prerequisites/validate_install.sh
+++ test_collections/matter/scripts/validate_th_instal_prerequisites/validate_install.sh
@@ -14,7 +14,6 @@
  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  # See the License for the specific language governing permissions and
  # limitations under the License.
-set -e
 
 # Paths
 TH_DIR="/app/certification-tool"
@@ -29,6 +28,7 @@ print_start_of_script
 
 print_script_step "Calling Install Docker Repository Script"
 $TH_DOCKER_REPO_INSTALL_FILE
+verify_return_code
 
 IFS=$(echo -en "\r")
 readarray th_package_list < "$TH_PACKAGE_LIST_FILE"
@@ -37,11 +37,15 @@ readarray matter_package_list < "$MATTER_PACKAGE_LIST_FILE"
 for package in ${th_package_list[@]}; do
     print_script_step "Installing package: ${package[@]}"
     DEBIAN_FRONTEND=noninteractive apt-get satisfy ${package[@]} -y --allow-downgrades
+    verify_return_code
 done
 
 for package in ${matter_package_list[@]}; do
     print_script_step "Installing package: ${package[@]}"
     DEBIAN_FRONTEND=noninteractive apt-get satisfy ${package[@]} -y --allow-downgrades
+    verify_return_code
 done
+
+print_installation_success
 
 print_end_of_script
