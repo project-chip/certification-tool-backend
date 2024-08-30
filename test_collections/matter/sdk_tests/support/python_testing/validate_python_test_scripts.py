9e6a8012a5fe58e44d107aa498c69dc70f74b59a
Romulo Quidute Filho <rquidute@apple.com>
2024-08-30 17:54:00 +0000

    Undo changes - Fix flake8

--- test_collections/matter/sdk_tests/support/python_testing/validate_python_test_scripts.py
+++ test_collections/matter/sdk_tests/support/python_testing/validate_python_test_scripts.py
@@ -2,11 +2,11 @@ import sys
 from pathlib import Path
 
 from app.test_engine.logger import test_engine_logger as logger
-from test_collections.matter.sdk_tests.support.python_testing.models.python_test_models import (  # noqa: E501
+from test_collections.matter.sdk_tests.support.python_testing.models.python_test_models import (
     PythonTest,
     PythonTestType,
 )
-from test_collections.matter.sdk_tests.support.python_testing.models.python_test_parser import (  # noqa: E501
+from test_collections.matter.sdk_tests.support.python_testing.models.python_test_parser import (
     parse_python_script,
 )
 
