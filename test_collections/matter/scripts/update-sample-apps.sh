c52f4474f3b06a23431e57d67fbb0260a64eb28d
Hilton Lima <hilton_lima@apple.com>
2024-09-09 20:30:22 +0000

    Isolate SDK docker pull script

--- test_collections/matter/scripts/update-sample-apps.sh
+++ test_collections/matter/scripts/update-sample-apps.sh
@@ -22,22 +22,6 @@ source "$TH_SCRIPTS_DIR/utils.sh"
 
 print_start_of_script
 
-# We are fetching SDK docker image and tag name from backend
-# This is done to minimize the places the SDK version is tracked.
-SDK_DOCKER_PACKAGE=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_IMAGE | cut -d'"' -f 2 | cut -d"'" -f 2)
-SDK_DOCKER_TAG=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_TAG | cut -d'"' -f 2 | cut -d"'" -f 2)
-SDK_DOCKER_IMAGE=$SDK_DOCKER_PACKAGE:$SDK_DOCKER_TAG
-
-DOCKER_IMAGE_FOUND=$(sudo docker images -q $SDK_DOCKER_IMAGE)
-
-if [[ -z "$DOCKER_IMAGE_FOUND" ]]; then
-    print_script_step "Pulling '$SDK_DOCKER_IMAGE' image"
-    sudo docker pull $SDK_DOCKER_IMAGE
-else
-    echo "SDK Docker image already exists"
-    echo "$SDK_DOCKER_IMAGE"
-fi
-
 print_script_step "Updating Sample APPs"
 # TODO: update SDK image to place the apps in a specific folder and then copy that entire folder
 sudo docker run -t -v ~/apps:/apps $SDK_DOCKER_IMAGE bash -c "rm -v /apps/*; cp -v chip-* /apps/; cp -v thermostat-app /apps/; cp -v lit-icd-app /apps/;cp -v fabric-* /apps/; cp -v matter-network-manager-app /apps/"
