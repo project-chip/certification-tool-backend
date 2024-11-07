73380fc62387a6d3fe9f72736451e3e3aaadf4b9
hiltonlima <116589806+hiltonlima@users.noreply.github.com>
2024-11-07 09:49:01 -0300

    Revert "[FIX] Intermittent Tests Start Error With No Information (#157)" (#166)
    This reverts commit 316a934d45d94f177c3525d3d9cd8b6718e81014.

--- test_collections/matter/sdk_tests/support/python_testing/models/test_case.py
+++ test_collections/matter/sdk_tests/support/python_testing/models/test_case.py
@@ -17,6 +17,7 @@ import re
 from asyncio import sleep
 from enum import IntEnum
 from inspect import iscoroutinefunction
+from multiprocessing.managers import BaseManager
 from pathlib import Path
 from socket import SocketIO
 from typing import Any, Optional, Type, TypeVar
@@ -37,7 +38,10 @@ from ...pics import PICS_FILE_PATH
 from ...sdk_container import SDKContainer
 from ...utils import prompt_for_commissioning_mode
 from .python_test_models import PythonTest, PythonTestType
-from .python_testing_hooks_proxy import SDKPythonTestResultBase
+from .python_testing_hooks_proxy import (
+    SDKPythonTestResultBase,
+    SDKPythonTestRunnerHooks,
+)
 from .utils import (
     EXECUTABLE,
     RUNNER_CLASS_PATH,
@@ -257,7 +261,9 @@ class PythonTestCase(TestCase, UserPromptSupport):
         try:
             logger.info("Running Python Test: " + self.python_test.name)
 
-            manager = self.sdk_container.manager
+            BaseManager.register("TestRunnerHooks", SDKPythonTestRunnerHooks)
+            manager = BaseManager(address=("0.0.0.0", 50000), authkey=b"abc")
+            manager.start()
             test_runner_hooks = manager.TestRunnerHooks()  # type: ignore
 
             if not self.python_test.path:
