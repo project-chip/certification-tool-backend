f440821bb27e810712641eb7084685d0a3796faf
Romulo Quidute Filho <116586593+rquidute@users.noreply.github.com>
2024-11-19 12:02:33 -0300

    Fix move update-pull-sdk-docker-image script content to update-sample-app script (#169)

--- test_collections/matter/scripts/update-sample-apps.sh
+++ test_collections/matter/scripts/update-sample-apps.sh
@@ -23,10 +23,29 @@ source "$TH_SCRIPTS_DIR/utils.sh"
 print_start_of_script
 
 print_script_step "Pulling chip-cert-bins docker image"
-$MATTER_PROGRAM_DIR/scripts/update-pull-sdk-docker-image.sh
+
+# We are fetching SDK docker image and tag name from backend
+# This is done to minimize the places the SDK version is tracked.
+SDK_DOCKER_PACKAGE=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_IMAGE | cut -d'"' -f 2 | cut -d"'" -f 2)
+SDK_DOCKER_TAG=$(cat $MATTER_PROGRAM_DIR/config.py | grep SDK_DOCKER_TAG | cut -d'"' -f 2 | cut -d"'" -f 2)
+SDK_DOCKER_IMAGE=$SDK_DOCKER_PACKAGE:$SDK_DOCKER_TAG
+
+
+DOCKER_IMAGE_FOUND=$(sudo docker images -q $SDK_DOCKER_IMAGE)
+
+if [[ -z "$DOCKER_IMAGE_FOUND" ]]; then
+    print_script_step "Pulling '$SDK_DOCKER_IMAGE' image"
+    sudo docker pull $SDK_DOCKER_IMAGE
+else
+    echo "SDK Docker image already exists"
+    echo "$SDK_DOCKER_IMAGE"
+fi
+
 
 print_script_step "Updating Sample APPs"
-sudo docker run -t -v ~/apps:/apps $SDK_DOCKER_IMAGE bash -c "rm -v /apps/*; cp -v apps/* /apps/"
+# TODO - Uncomment line bellow and remove the subsequent line when the SDK IMAGE contains the apps folder
+# sudo docker run -t -v ~/apps:/apps $SDK_DOCKER_IMAGE bash -c "rm -v /apps/*; cp -v apps/* /apps/"
+sudo docker run -t -v ~/apps:/apps $SDK_DOCKER_IMAGE bash -c "rm -v /apps/*; cp -v chip-* /apps/; cp -v thermostat-app /apps/; cp -v lit-icd-app /apps/;cp -v fabric-* /apps/; cp -v matter-network-manager-app /apps/"
 echo "Setting Sample APPs ownership"
 sudo chown -R `whoami` ~/apps
 
