fe2a361bbe99c03e8728041173ef8cb0e85e2a75
Romulo Quidute Filho <116586593+rquidute@users.noreply.github.com>
2025-04-01 13:56:11 -0300

    Updated v2.12+spring2025 release (#208)
    * Updated SDK_SHA for v1.4.1 release
    
    * Updated SDK SHA 0c90d7299f7ab70b325e8a7febd0f210ae629ce4

--- test_collections/matter/config.py
+++ test_collections/matter/config.py
@@ -23,9 +23,9 @@ class MatterSettings(BaseSettings):
 
     # SDK Docker Image
     SDK_DOCKER_IMAGE: str = "connectedhomeip/chip-cert-bins"
-    SDK_DOCKER_TAG: str = "91eab26d458b53df5be0b3547bef7b2796270184"
+    SDK_DOCKER_TAG: str = "0c90d7299f7ab70b325e8a7febd0f210ae629ce4"
     # SDK SHA: used to fetch tests (YAML and Python) from SDK.
-    SDK_SHA: str = "91eab26d458b53df5be0b3547bef7b2796270184"
+    SDK_SHA: str = "0c90d7299f7ab70b325e8a7febd0f210ae629ce4"
 
     class Config:
         case_sensitive = True
