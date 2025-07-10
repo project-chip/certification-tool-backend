5f9487481034ed8e919590e0557dbb965f98a1a7
chulspro <chulspro.kim@samsung.com>
2025-07-10 22:17:56 +0900

    [Camera] Handle video stream socket operations in SocketConnectionManager (#241)
    * Handle video stream socket operations in SocketConnectionManager
    
    Signed-off-by: s-gatti <s.gatti@samsung.com>
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>
    
    * Update imports and fixes
    
    Signed-off-by: s-gatti <s.gatti@samsung.com>
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>
    
    * review fixes
    
    Signed-off-by: s-gatti <s.gatti@samsung.com>
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>
    
    ---------
    
    Signed-off-by: s-gatti <s.gatti@samsung.com>
    Signed-off-by: Charles Kim <chulspro.kim@samsung.com>
    Co-authored-by: s-gatti <s.gatti@samsung.com>

--- test_collections/matter/sdk_tests/support/yaml_tests/models/test_case.py
+++ test_collections/matter/sdk_tests/support/yaml_tests/models/test_case.py
@@ -26,6 +26,7 @@ from app.test_engine.models import (
 from app.test_engine.models.test_case import CUSTOM_TEST_IDENTIFIER
 
 from ...chip.chip_server import ChipServerType
+from ...constants import USER_ACTIONS
 from ...models.matter_test_models import MatterTestStep, MatterTestType
 from ...yaml_tests.models.chip_test import ChipManualPromptTest, ChipTest
 from .yaml_test_models import YamlTest
@@ -175,11 +176,7 @@ class YamlTestCase(TestCase):
             return
 
         step = TestStep(yaml_step.label)
-        if yaml_step.command in [
-            "UserPrompt",
-            "PromptWithResponse",
-            "VerifyVideoStream",
-        ]:
+        if yaml_step.command in USER_ACTIONS:
             step = ManualVerificationTestStep(
                 name=yaml_step.label,
                 verification=yaml_step.verification,
--- test_collections/matter/sdk_tests/support/yaml_tests/models/yaml_test_parser.py
+++ test_collections/matter/sdk_tests/support/yaml_tests/models/yaml_test_parser.py
@@ -18,6 +18,7 @@ from pathlib import Path
 from loguru import logger
 from pydantic import ValidationError
 
+from ...constants import USER_ACTIONS
 from ...models.matter_test_models import MatterTestType
 from .yaml_test_models import YamlTest
 
@@ -52,10 +53,7 @@ def _test_type(test: YamlTest) -> MatterTestType:
 
     # if any step has a UserPrompt, PromptWithResponse or VerifyVideoStream command,
     # categorize as semi-automated
-    if any(
-        s.command in ["UserPrompt", "PromptWithResponse", "VerifyVideoStream"]
-        for s in steps
-    ):
+    if any(s.command in USER_ACTIONS for s in steps):
         return MatterTestType.SEMI_AUTOMATED
 
     # Otherwise Automated
