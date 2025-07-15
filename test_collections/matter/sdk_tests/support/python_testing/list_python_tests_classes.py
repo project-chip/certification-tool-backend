980aa54ee8e04a4763adc571a17f91618d655088
Romulo Quidute Filho <rquidute@apple.com>
2025-07-15 19:21:14 +0000

    Code review

--- test_collections/matter/sdk_tests/support/python_testing/list_python_tests_classes.py
+++ test_collections/matter/sdk_tests/support/python_testing/list_python_tests_classes.py
@@ -186,8 +186,10 @@ async def __process_grouped_commands(
             if "info" not in json_dict and "script_path" in json_dict:
                 errors_found.append(
                     f"Failed running command: {command_string}.\n"
-                    f"Error message details: {json_dict.get('detail', 'No detail provided')}\n"
-                    f"Error message script_path: {json_dict['script_path']}"
+                    "Error message details: "
+                    f"{json_dict.get('detail', 'No detail provided')}\n"
+                    "Error message script_path: "
+                    f"{json_dict.get('script_path', 'No script path provided')}\n"
                 )
                 invalid_test_function_count += 1
                 continue
