97bdfeed7328d86cbc50265f62a3375315a0705b
Romulo Quidute Filho <116586593+rquidute@users.noreply.github.com>
2025-08-15 17:57:37 -0300

    Log the PICS config used in test run (#21)
    * Log the PICS ocnfig used in test run
    
    * Apply Json indentation

--- app/commands/run_tests_cli.py
+++ app/commands/run_tests_cli.py
@@ -86,6 +86,7 @@ async def run_tests_cli(title: str, config: str, tests_list: str, pics_config_fo
 
     # Read PICS configuration if provided
     pics = read_pics_config(pics_config_folder)
+    click.echo(f"PICS Used: {json.dumps(pics, indent=2)}")
 
     try:
         # Convert each test separeted by comma to a list
