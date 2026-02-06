055eae9e2dcd8a4d7ed0e7ed83aa66f064c85cff
ArnoldBraine <arnold.braine@gmail.com>
2026-02-06 09:59:33 +0100

    reverting change in test_suite.py

--- test_collections/matter/sdk_tests/support/python_testing/models/test_suite.py
+++ test_collections/matter/sdk_tests/support/python_testing/models/test_suite.py
@@ -139,10 +139,7 @@ class CommissioningPythonTestSuite(PythonTestSuite, UserPromptSupport):
         if self.matter_config.dut_config.pairing_mode in (
             DutPairingModeEnum.BLE_THREAD,
             DutPairingModeEnum.NFC_THREAD,
-        ) and isinstance(
-            self.matter_config.network.thread, 
-            ThreadAutoConfig
-        ):
+        ) and isinstance(self.matter_config.network.thread, ThreadAutoConfig):
             await self.border_router.start_device(self.matter_config.network.thread)
             await self.border_router.form_thread_topology()
 
