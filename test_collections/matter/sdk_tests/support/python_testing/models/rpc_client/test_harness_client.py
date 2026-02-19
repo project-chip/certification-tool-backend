9e816e78882441a34852f592b3ee275cbb8d1bcb
Romulo Quidute Filho <rquidute@apple.com>
2026-02-19 14:27:53 +0000

    Fixed imports in test_harness_client.py file

--- test_collections/matter/sdk_tests/support/python_testing/models/rpc_client/test_harness_client.py
+++ test_collections/matter/sdk_tests/support/python_testing/models/rpc_client/test_harness_client.py
@@ -24,8 +24,8 @@ from contextlib import redirect_stdout
 from multiprocessing.managers import BaseManager
 
 from matter.testing.commissioning import CommissionDeviceTest
-from matter.testing.matter_testing import (
-    MatterTestConfig,
+from matter.testing.matter_testing import MatterTestConfig
+from matter.testing.runner import (
     TestStep,
     get_test_info,
     parse_matter_test_args,
