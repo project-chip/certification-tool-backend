2b3dfab85a697362a86673e42fb7c6217f6b7817
Romulo Quidute Filho <rquidute@apple.com>
2026-05-05 22:45:16 +0000

    Fixed missing f-string

--- test_collections/matter/test_environment_config.py
+++ test_collections/matter/test_environment_config.py
@@ -121,7 +121,7 @@ class TestEnvironmentConfigMatter(TestEnvironmentConfig):
                     if not dut_config.get(field):
                         raise TestEnvironmentConfigMatterError(
                             f"The field {field} is required for dut_config "
-                            "configuration when pairing_mode is {pairing_mode}"
+                            f"configuration when pairing_mode is {pairing_mode}"
                         )
 
             # Validate THREAD_MESHCOP mode requires ba_host and ba_port
