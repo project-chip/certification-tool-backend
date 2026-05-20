4733747db568d5a5d34868ac9be20e733d051341
Romulo Quidute Filho <rquidute@apple.com>
2026-05-20 20:48:06 +0000

    v2.15+spring2026 version

--- test_collections/matter/config.py
+++ test_collections/matter/config.py
@@ -23,9 +23,9 @@ class MatterSettings(BaseSettings):
 
     # SDK Docker Image
     SDK_DOCKER_IMAGE: str = "connectedhomeip/chip-cert-bins"
-    SDK_DOCKER_TAG: str = "6feac778f196483b6355d35fc529f183b293b71f"
+    SDK_DOCKER_TAG: str = "ad9cf715421da92c3e6fb17c3086b900cf281c5b"
     # SDK SHA: used to fetch tests (YAML and Python) from SDK.
-    SDK_SHA: str = "6feac778f196483b6355d35fc529f183b293b71f"
+    SDK_SHA: str = "ad9cf715421da92c3e6fb17c3086b900cf281c5b"
 
     class Config:
         case_sensitive = True
