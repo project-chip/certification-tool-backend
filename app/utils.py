83a8a600265c6a236552617624243abb3723dcd4
Steven Green <steven.green@smartthings.com>
2026-09-09 13:14:35 -0700

    missed import remove

--- app/utils.py
+++ app/utils.py
@@ -25,7 +25,6 @@ from typing import IO, Optional, Tuple, Type
 
 from loguru import logger
 
-from app.core.config import settings
 from app.models import TestRunExecution
 from app.schemas import TestSelection
 
