859d29770549cb20cfba7772c4621d74bfac0227
Steven Green <sgreen@csa-iot.org>
2026-09-10 10:20:28 -0700

    [Cleanup] Remove unused code and dependencies (#367)
    * [Cleanup] Remove unused code and dependencies
    These e-mail utilities seem to have never been used, or at least are no longer used. This allows us to cut out a fair number of backend dependencies.
    
    * missed import remove

--- app/core/config.py
+++ app/core/config.py
@@ -13,17 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import secrets
 from typing import Any, Dict, List, Optional, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, PostgresDsn, validator
+from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
-    # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
@@ -59,34 +55,6 @@ class Settings(BaseSettings):
             path=f"/{values.get('POSTGRES_DB') or ''}",
         )
 
-    SMTP_TLS: bool = True
-    SMTP_PORT: Optional[int] = None
-    SMTP_HOST: Optional[str] = None
-    SMTP_USER: Optional[str] = None
-    SMTP_PASSWORD: Optional[str] = None
-    EMAILS_FROM_EMAIL: Optional[EmailStr] = None
-    EMAILS_FROM_NAME: Optional[str] = None
-
-    @validator("EMAILS_FROM_NAME")
-    def get_project_name(cls, v: Optional[str], values: Dict[str, Any]) -> str:
-        if not v:
-            return values["PROJECT_NAME"]
-        return v
-
-    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
-    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
-    EMAILS_ENABLED: bool = False
-
-    @validator("EMAILS_ENABLED", pre=True)
-    def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
-        return bool(
-            values.get("SMTP_HOST")
-            and values.get("SMTP_PORT")
-            and values.get("EMAILS_FROM_EMAIL")
-        )
-
-    EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
-
     # Logging
     LOGGING_PATH: str = "./logs"
     LOGGING_FILENAME: str = "{time:YYYY-MM-DD}.log"
@@ -99,13 +67,6 @@ class Settings(BaseSettings):
         "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
         "<level>{message}</level>"
     )
-
-    # Email Crash Notifier
-    NOTIFIER_ENABLE_NOTIFICATIONS: bool = False
-    NOTIFIER_USERNAME: str = "xiole.chip.test@gmail.com"
-    NOTIFIER_PASSWORD: str = "yntwdxwyofvehafg"  # spell-checker:disable-line
-    NOTIFIER_TO: str = "xiole.chip.test@gmail.com"
-    NOTIFIER_SUBJECT: str = "CHIP Tool Crash Log"
 
     # Python Test Logging
     ENABLE_REALTIME_PYTHON_TEST_LOGS: bool = False
